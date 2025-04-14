8da7d21edb8ac9df73aa45746e0c7a92fee16202
Frederico Regateiro <frederico.regateiro@outlook.com>
2025-04-14 23:51:19 +0100

    feat: Implement Select and Tabs components with TypeScript support
    - Added SelectItem, SelectItemText, SelectLabel, SelectScrollDownButton, SelectScrollUpButton, SelectSeparator, SelectTrigger, and SelectValue components.
    - Integrated TypeScript for type safety and improved developer experience.
    - Enhanced Tabs component with TabsRoot, TabsContent, TabsList, and TabsTrigger for better UI organization.
    - Introduced utility functions for class name management and props forwarding.
    - Created models for beneficiaries, concelhos, and distritos to manage data effectively.

---
+++ resources/js/models/beneficiarios.js
@@ -0,0 +1,10 @@
+const beneficiarios = [
+    { id: 1, key: 'beneficiario1', nome: 'Empresa' },
+    { id: 2, key: 'beneficiario2', nome: 'Particular' },
+    { id: 3, key: 'beneficiario3', nome: 'Entidade pública ou equiparada' },
+    { id: 4, key: 'beneficiario4', nome: 'Entidade sem fins lucrativos' },
+    { id: 5, key: 'beneficiario6', nome: 'IPSS' },
+    { id: 6, key: 'beneficiario7', nome: 'Entidade formadora' }
+]
+
+export default beneficiarios
